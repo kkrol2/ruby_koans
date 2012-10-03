cf08ae6aee07117c98a1a416c6b026e5e5c80f48
unknown <C:\Users\user\AppData\Roaming\The Bat!>
2012-10-03 11:17:18 +0200

    commit

--- src/about_dice_project.rb
+++ src/about_dice_project.rb
@@ -6,15 +6,6 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 #   code ...
 # end
 
-#--
-class DiceSet
-  attr_reader :values
-  def roll(n)
-    @values = (1..n).map { rand(6) + 1 }
-  end
-end
-
-#++
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
